0484d629a8c046547b3d08561c2e386e43ed9483
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-04-11 20:20:11 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,6 @@
 def slice_simple():
-    texto = "Awesome"
+    texto=input("")
+print(texto[4::2])
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
