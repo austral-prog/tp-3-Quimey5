094ddac3e85cd09cab951e855eaa60182e442b58
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-04-11 19:10:09 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,9 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-
+txt="Programming"
+print(txt.lower())
+print(txt.lower()[0:3])
+print(txt.lower()[3:6])
+print(txt.lower()[0:])
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
