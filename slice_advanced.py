a41b675b0e1a325ff8d018cf0bfd8d13e01024aa
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-04-12 11:54:11 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,8 +1,9 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-txt=input("").lower()
-print(txt.lower()[0:3])
-print(txt.lower()[3:6])
-print(txt.lower()[0:])
+texto="Awesome"
+texto=input("").lower()
+print(txeto.lower()[0:3])
+print(txto.lower()[3:6])
+print(txto.lower()[0:])
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
