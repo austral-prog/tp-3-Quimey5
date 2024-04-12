b34a8a18065d7883d03c4353f692543d6cd79ae0
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-04-12 12:09:33 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -4,6 +4,6 @@ texto="Awesome"
 texto=input("").lower()
 print(txeto.lower()[0:3])
 print(txto.lower()[2:5])
-print(txto.lower()[0:])
+print(texto.lower()[0:4] +(texto.lower()) [-3:])
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
